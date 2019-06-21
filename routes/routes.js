f1e10effca12d7cd20252864243616ac0390830e
malhamah <andreferi135@gmail.com>
2019-06-21 22:21:40 +0700

    new model update

--- routes/routes.js
+++ routes/routes.js
@@ -2,38 +2,20 @@
 // author : andreferi
 
 module.exports = (app) => {
-    const noteController = require("../controller/notesController");
-    const categoryController = require("../controller/categoryController");
-    const appController = require("../controller/appController");
     const categoriesController = require("../controller/categoriesController");
     const note = require("../controller/noteController");
 
     // GET NOTES
-    app.get("/", noteController.home);
-    app.get("/notes", noteController.allNotes);
-    app.get("/notes/:id", noteController.notes);
-    app.post("/notes", noteController.addNote);
-    app.patch("/notes/:id", noteController.editNote);
-    app.delete("/notes/:id", noteController.deleteNote);
-
     app.get("/note", note.allData);
+    app.get("/note/:id", note.getOneNote);
     app.post("/note", note.addNote);
     app.patch("/note/:id", note.updNote);
     app.delete("/note/:id", note.delNote);
-    app.get("/note/:id", note.getOneNote);
 
     // GET CATEGORIES
-    app.get("/", categoryController.home);
-    app.get("/category", categoryController.allCat);
-    app.get("/category/:id", categoryController.category);
-    app.post("/category", categoryController.addCat);
-    app.patch("/category/:id", categoryController.editCat);
-    app.delete("/category/:id", categoryController.deleteCat);
-    app.get("/note/categories/:id", appController.noteByCatId);
-
     app.get("/categories", categoriesController.get);
+    app.get("/note/category/:id", categoriesController.getByCategory);
     app.post("/categories", categoriesController.add);
     app.patch("/categories/:id", categoriesController.update);
     app.delete("/categories/:id", categoriesController.delete);
-    app.get("/note/category/:id", categoriesController.getByCategory);
 }
